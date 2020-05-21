e815e5085ce950219bf17d0be6ec7442acd6d9f0
Blake Sykes <blakesykes13@gmail.com>
2020-05-21 13:52:52 -0600

    Clean up the next.js starting template. Installed Tone.js and Styled-Components. Started on file structure for components.

---
+++ components/Layout/Container/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin-right: 10%;
+    margin-left: 10%;
+    color: red;
+    background-color: black;
+    `;
+
+export default Container;
